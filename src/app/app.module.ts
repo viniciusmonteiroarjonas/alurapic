20e5ea6cb13a6d4262b84bba808ddc901527b35e
Vinicius Monteiro Arjonas <viniciusmonteiroarjonas@outlook.com>
2020-02-06 17:56:04 -0300

    Criado o componente de photos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    PhotosModule,
     AppRoutingModule
   ],
   providers: [],
