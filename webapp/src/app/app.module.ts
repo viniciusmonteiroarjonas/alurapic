5b8de1bc9c35b4af4f0d3072b36ad5f3d3c09c66
Vinicius Monteiro Arjonas <viniciusmonteiroarjonas@outlook.com>
2020-02-10 18:39:44 -0300

    Implementado autenticação

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { ErrorsModule } from './errors/errors.module';
 import { HomeModule } from './home/home.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HomeModule } from './home/home.module';
     PhotosModule,
     AppRoutingModule,
     ErrorsModule,
-    HomeModule
+    HomeModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- webapp/src/app/core/auth/auth.service.ts
+++ webapp/src/app/core/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { UserService } from '../user/user.service';
 
 const API_URL = 'http://localhost:3000'
 
@@ -10,11 +12,15 @@ export class AuthService {
 
   constructor(
     private http: HttpClient,
+    private userService: UserService
   ) { }
 
   authenticate(userName: string, password: string) {
 
-    return this.http.post(`${API_URL}/user/login`, { userName, password })
-
+    return this.http.post(`${API_URL}/user/login`, { userName, password }, { observe: 'response' })
+      .pipe(tap(res => {
+        const authToken = res.headers.get('x-access-token');
+        this.userService.setToken(authToken);
+      }));
   }
 }
