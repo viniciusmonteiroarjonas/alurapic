6baa0e1428ed906dec6e57364fd23bb4c57a51e0
Vinicius Monteiro Arjonas <viniciusmonteiroarjonas@outlook.com>
2020-02-11 20:20:36 -0300

    proteção de rotas usuário logado

--- webapp/src/app/core/user/user.service.ts
+++ webapp/src/app/core/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { TokenService } from '../token/token.service';
-import { Subject } from 'rxjs';
+import { Subject, BehaviorSubject } from 'rxjs';
 import { User } from './user';
 import * as jwt_decode from 'jwt-decode';
 
@@ -8,7 +8,8 @@ import * as jwt_decode from 'jwt-decode';
   providedIn: 'root'
 })
 export class UserService {
-  private userSubject = new Subject<User>();
+  private userSubject = new BehaviorSubject<User>(null);
+  private userName: string;
 
   constructor(private tokenService: TokenService) {
     this.tokenService.hasToken() && this.decodeAndNotify();
@@ -26,6 +27,20 @@ export class UserService {
   private decodeAndNotify() {
     const token = this.tokenService.getToken();
     const user = jwt_decode(token) as User;
+    this.userName = user.name;
     this.userSubject.next(user);
+  }
+
+  logout() {
+    this.tokenService.removeToken();
+    this.userSubject.next(null);
+  }
+
+  isLogged() {
+    return this.tokenService.hasToken();
+  }
+
+  getUserName() {
+    return this.userName;
   }
 }
