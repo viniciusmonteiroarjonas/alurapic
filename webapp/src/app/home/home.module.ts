ef5c0e2949f9a7de401b24b4753c1ca1dc79d733
Vinicius Monteiro Arjonas <viniciusmonteiroarjonas@outlook.com>
2020-02-13 10:07:27 -0300

    registro de novos usuarios

--- webapp/src/app/home/home.module.ts
+++ webapp/src/app/home/home.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { SignInComponent } from './signin/signin.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { SignUpComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [SignInComponent],
-  imports: [ReactiveFormsModule, CommonModule, VMessageModule, RouterModule]
+  declarations: [SignInComponent, SignUpComponent],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule, VMessageModule, RouterModule]
 })
 export class HomeModule { }
---
+++ webapp/src/app/home/signup/user-not-taken.validator.service.ts
@@ -0,0 +1,24 @@
+import { debounceTime, switchMap, first, map } from 'rxjs/operators';
+import { AbstractControl } from '@angular/forms';
+import { SignUpService } from './signup.service';
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class UserNotTakenValidatorService {
+
+  constructor(private signUpService: SignUpService) { }
+
+  checkUserNameTaken() {
+
+    return (control: AbstractControl) => {
+      return control
+        .valueChanges
+        .pipe(debounceTime(300))
+        .pipe(switchMap(userName =>
+          this.signUpService.checkUserNameTaken(userName)
+        ))
+        .pipe(map(isTaken => isTaken ? { userNameTaken: true } : null))
+        .pipe(first());
+    }
+  }
+}
